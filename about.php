adfc71f6101815d761b283c1c99880ca957ca2ed
Yamina-Ghobni <95687757+Yamina-Ghobni@users.noreply.github.com>
2022-02-08 23:05:07 +0100

    Add files via upload

---
+++ about.php
@@ -0,0 +1,17 @@
+
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header" align="center"><font color="#0047AB	">Polytechnique FeedBack</font> </h1>
+                
+            </div>
+        </div>
+ 
+        <div class="row" style="margin-bottom:50px;margin-left:50px">
+           
+               <P>Polytechnique feedback System, to make feedbacks easier for everyone.
+</P> 
+               
+            </div>
+        </div>
+     
+     
